2c2b9ce3b7f434e44354de7634665a5760feaf45
Luka Lapanashvili <luka.l@protonmail.ch>
2026-01-15 15:24:19 +0100

    Update environment variables and improve update script for better error handling and logging

--- update.sh
+++ update.sh
@@ -1,47 +1,50 @@
 #!/bin/bash
-set -e
-
+set -Eeuo pipefail
 
 # Get the directory of the current script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $SCRIPT_DIR
-
+cd "$SCRIPT_DIR"
 
 key_file="./secrets/gh_key.txt"
 
 # Check if the key file exists
-if [ -f "$key_file" ]; then
-    gh_key=$(cat "$key_file")
+if [[ -f "$key_file" ]]; then
+    gh_key="$(<"$key_file")"
 else
     echo "Key file $key_file not found."
     exit 1
 fi
 
 # Login and pull latest docker images
-echo "[1/6] Pulling latest images..."
+echo "[1/7] Pulling latest images..."
 docker login --username Haeri --password-stdin ghcr.io <<< "$gh_key"
 docker compose pull
 
 # Backup volume
-echo "[2/6] Backing up volume..."
-rm -rf volume_backup/*
-cp -r volume/DataSpace_data volume_backup/DataSpace_data
+printf "\n[2/7] Backing up volume...\n"
+rsync -a --delete --info=progress2 -h \
+  volume/DataSpace_data/ \
+  volume_backup/DataSpace_data/
 
-# Start all docker services
-echo "[3/6] Starting Docker services..."
-./startup.sh
+# Start database
+printf "\n[3/7] Starting Database...\n"
+docker compose --file docker-compose.yml --env-file .env up database -d
+sleep 10
 
-# Create Backup
-echo "[4/6] Backing up database..."
+# Create database backup
+printf "\n[4/7] Backing up database...\n"
 docker exec DataSpace_pgbackups ./backup.sh
 
-# Install database
-echo "[5/6] Installing database migrations..."
+# Install database migrations
+printf "\n[5/7] Installing database migrations...\n"
 docker exec DataSpace_api ./DataSpaceMigration
 
+# Start all docker services
+printf "\n[6/7] Starting all Docker services...\n"
+./startup.sh
+
 # Clean up unused Docker resources
-echo "[6/6] Cleaning up unused Docker resources..."
+printf "\n[7/7] Cleaning up unused Docker resources...\n"
 docker image prune -a -f
 docker builder prune -f
-docker volume prune -f
 docker container prune -f
